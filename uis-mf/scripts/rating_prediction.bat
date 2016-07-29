61c0d0a5ac2359d981fd363bf34fb96fe6ce545e
poodarchu <poodarchu@gmail.com>
2016-07-30 00:54:14 +0800

    update

--- uis-mf/scripts/rating_prediction.bat
+++
@@ -1,7 +0,0 @@
-java -Xms1024m -Xmx1024m -cp ../build/mymedialite.jar RatingPrediction ^
-  --recommender=MatrixFactorization ^
-  --data-dir=C:\dataset\ ^
-  --training-file=ratings.csv ^
-  --test-ratio=0.2
-
-pause
